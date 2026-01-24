8aa172daf41fe764276a4c02bb019f95d4486fd7
sai <saisamireddy01@gmail.com>
2026-01-24 10:11:16 +0530

    feat: add JWT-authenticated WebSocket support

--- tasks/consumers.py
+++ tasks/consumers.py
@@ -3,13 +3,19 @@ import json
 
 class EchoConsumer(AsyncWebsocketConsumer):
     async def connect(self):
+        user = self.scope.get("user")
+
+        if not user or user.is_anonymous:
+            await self.close(code=4001)
+            return
+
         await self.accept()
 
     async def receive(self, text_data=None, bytes_data=None):
-        if text_data:
-            await self.send(text_data=json.dumps({
-                "echo": text_data
-            }))
+        await self.send(text_data=json.dumps({
+            "user": self.scope["user"].username,
+            "echo": text_data
+        }))
 
     async def disconnect(self, close_code):
         pass
