f838e9c092befa22a77ef8401e25675eb57e196d
sai <saisamireddy01@gmail.com>
2026-02-12 18:04:20 +0530

    feat(auth): implement user registration endpoint and validation logic

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,11 @@
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet
+from .views import UserViewSet, RegisterView
+from django.urls import path
 
 router = DefaultRouter()
 router.register(r"users", UserViewSet, basename="users")
+urlpatterns = [
+    path("register/", RegisterView.as_view(), name="register"),
+]
 
-urlpatterns = router.urls
+urlpatterns += router.urls
